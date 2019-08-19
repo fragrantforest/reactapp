d7ee061e65b60cf807334ba142646500add0acc2
lpf <lpf@yfyun.net>
2019-08-19 17:44:13 +0800

    tag test

--- src/app.jsx
+++ src/app.jsx
@@ -18,7 +18,7 @@ import OrderList        from 'page/order/index.jsx';
 import OrderDetail      from 'page/order/detail.jsx';
 import UserList         from 'page/user/index.jsx';
 import ErrorPage        from 'page/error/index.jsx';
-
+//增加我后要打tag
 class App extends React.Component{
     render(){
         let LayoutRouter = (
